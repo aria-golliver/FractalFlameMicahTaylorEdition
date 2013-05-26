c466a4217024a53dbee16aed1f914d1a1a8ad467
Aaron Golliver <aaron.golliver@gmail.com>
2013-05-25 22:17:47 -0400

    GLFW window opens, need to figure out how to write a texture to it

--- FractalFlameMicahTaylorEdition/fractaldisplay.c
+++ FractalFlameMicahTaylorEdition/fractaldisplay.c
@@ -1,5 +1,25 @@
-void displayinit(void){
+#include "fractaldisplay.h"
+#include "GL\glfw.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+void displayinit(void){
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    // Open a window and create its OpenGL context
+    if( !glfwOpenWindow( 640, 480, 0,0,0,0, 0,0, GLFW_WINDOW ) )
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    glfwSetWindowTitle( "Spinning Triangle" );
 }
 void updateDisplay(void){
 
